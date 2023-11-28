383b28adf8c86f75b339118dee40e9233fdaa56b
NicolasLi <linze201@gmail.com>
2023-11-28 12:21:26 +0800

    Function Composition

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import { produce } from 'immer';
-let book = { title: 'Harry Potter' };
+// import { produce } from 'immer';
+// let book = { title: 'Harry Potter' };
 
-function publish(book) {
-  return produce(book, (draftBook) => {
-    draftBook.isPublished = true;
-  });
-}
+// function publish(book) {
+//   return produce(book, (draftBook) => {
+//     draftBook.isPublished = true;
+//   });
+// }
 
-let updated = publish(book);
-console.log(book);
-console.log(updated);
+// let updated = publish(book);
+// console.log(book);
+// console.log(updated);
+
+let input = '    JavaScript     ';
+let output = '<div>' + input.trim() + '</div>';
+
+const trim = (str) => str.trim();
+const wrapInDiv = (str) => `<div>${str}</div>`;
+const toLowerCase = (str) => str.toLowerCase();
+
+const result = wrapInDiv(toLowerCase(trim(input)));
