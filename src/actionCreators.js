b1b03a41e0dde3f7f682092310047481e56ea9aa
NicolasLi <linze201@gmail.com>
2023-11-28 20:07:15 +0800

    add bugResolved action

--- src/actionCreators.js
+++ src/actionCreators.js
@@ -1,17 +1,15 @@
 import * as actions from './actionTypes';
 
-// export function bugAdded(description) {
-//   return {
-//     type: actions.BUG_ADDED,
-//     payload: {
-//       description: description,
-//     },
-//   };
-// }
-
 export const bugAdded = (description) => ({
   type: actions.BUG_ADDED,
   payload: {
     description: description,
+  },
+});
+
+export const bugResolved = (id) => ({
+  type: actions.BUG_RESOLVED,
+  payload: {
+    id,
   },
 });
--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import store from './store';
 import * as actions from './actionTypes';
-import { bugAdded } from './actionCreators';
-
-const unsubscribe = store.subscribe(() => {
-  console.log('Store changed!', store.getState()); // when store changed, get store state
-});
+import { bugAdded, bugResolved } from './actionCreators';
 
 store.dispatch(bugAdded('Bug 1'));
-
-unsubscribe(); // the second time we are dispatching an action, we don't get notified
-
-store.dispatch({
-  type: actions.BUG_REMOVED,
-  payload: {
-    id: 1,
-  },
-});
+store.dispatch(bugResolved(1));
 
 console.log(store.getState());
