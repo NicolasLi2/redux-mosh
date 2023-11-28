e63686d52dc9107f5c8567535ba548745928b783
NicolasLi <linze201@gmail.com>
2023-11-28 19:43:17 +0800

    Subscribing to the Store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import store from './store';
 
+const unsubscribe = store.subscribe(() => {
+  console.log('Store changed!', store.getState()); // when store changed, get store state
+});
+
 store.dispatch({
   type: 'bugAdded',
   payload: {
     description: 'Bug1',
   },
 });
+
+unsubscribe(); // the second time we are dispatching an action, we don't get notified
 
 store.dispatch({
   type: 'bugRemoved',
