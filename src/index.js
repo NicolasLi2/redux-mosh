ea2bd953971a549f23eee4a85b2cda55122a2213
NicolasLi <linze201@gmail.com>
2023-11-28 19:56:46 +0800

    add actionCreators.js file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import store from './store';
 import * as actions from './actionTypes';
+import { bugAdded } from './actionCreators';
 
 const unsubscribe = store.subscribe(() => {
   console.log('Store changed!', store.getState()); // when store changed, get store state
 });
 
-store.dispatch({
-  type: actions.BUG_ADDED,
-  payload: {
-    description: 'Bug1',
-  },
-});
+store.dispatch(bugAdded('Bug 1'));
 
 unsubscribe(); // the second time we are dispatching an action, we don't get notified
 
