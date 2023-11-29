52bf6817398c8bd254ed67d17f96c3c93a52ca65
NicolasLi <linze201@gmail.com>
2023-11-29 16:50:15 +0800

    create user slice and get bugs by user id

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -7,6 +7,11 @@ const slice = createSlice({
   name: 'bugs',
   initialState: [],
   reducers: {
+    bugAssignedToUser: (bugs, action) => {
+      const { bugId, userId } = action.payload;
+      const index = bugs.findIndex((bug) => bug.id === bugId);
+      bugs[index].userId = userId;
+    },
     bugAdded: (bugs, action) => {
       bugs.push({
         id: ++lastId,
@@ -21,7 +26,7 @@ const slice = createSlice({
   },
 });
 
-export const { bugAdded, bugResolved } = slice.actions;
+export const { bugAdded, bugResolved, bugAssignedToUser } = slice.actions;
 export default slice.reducer;
 
 // // Selector
@@ -36,3 +41,9 @@ export const getUnresolvedBugs = createSelector(
   // if the list of bugs and projects remain unchanged, this logic is not going to be recalculated again
   (bugs, projects) => bugs.filter((bug) => !bug.resolved)
 );
+
+export const getBugsByUser = (userId) =>
+  createSelector(
+    (state) => state.entities.bugs,
+    (bugs) => bugs.filter((bug) => bug.userId === userId)
+  );
