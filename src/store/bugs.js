e76080e907ac030afcd167b769653ab80e3b6ce0
NicolasLi <linze201@gmail.com>
2023-11-29 15:00:59 +0800

    use redux-toolkit to create reducer

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,4 +1,4 @@
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
 // Action creator
 export const bugAdded = createAction('bugAdded');
@@ -8,24 +8,19 @@ export const bugRemoved = createAction('bugRemoved');
 // reducer
 let lastId = 0;
 
-export default function reducer(state = [], action) {
-  switch (action.type) {
-    case bugAdded.type:
-      return [
-        ...state,
-        {
-          id: ++lastId,
-          description: action.payload.description,
-          resolved: false,
-        },
-      ];
-    case bugRemoved.type:
-      return state.filter((bug) => bug.id !== action.payload.id);
-    case bugResolved.type:
-      return state.map((bug) =>
-        bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
-      );
-    default:
-      return state;
-  }
-}
+export default createReducer([], {
+  // key: value
+  // actions: functions
+  [bugAdded.type]: (state, action) => {
+    state.push({
+      id: ++lastId,
+      description: action.payload.description,
+      resolved: false,
+    });
+  },
+
+  [bugResolved.type]: (state, action) => {
+    const index = state.findIndex((bug) => bug.id === action.payload.id);
+    state[index].resolved = true;
+  },
+});
