8782c9e0d563710d339c7e3d5a67897ade539eb1
NicolasLi <linze201@gmail.com>
2023-11-29 21:15:15 +0800

    API Middleware

--- src/index.js
+++ src/index.js
@@ -1,48 +1,20 @@
 import configureStore from './store/configureStore';
-// import {
-//   bugAdded,
-//   bugResolved,
-//   getUnresolvedBugs,
-//   bugAssignedToUser,
-//   getBugsByUser,
-// } from './store/bugs';
-// import { projectAdded } from './store/projects';
-// import { userAdded } from './store/users';
-
-// const store = configureStore();
-
-// store.subscribe(() => {
-//   console.log('Store changed!');
-// });
-
-// store.dispatch(userAdded({ name: 'User 1' }));
-// store.dispatch(userAdded({ name: 'User 2' }));
-// store.dispatch(projectAdded({ name: 'Project 1' }));
-// store.dispatch(bugAdded({ description: 'Bug1' }));
-// store.dispatch(bugAdded({ description: 'Bug2' }));
-// store.dispatch(bugAdded({ description: 'Bug3' }));
-// store.dispatch(bugResolved({ id: 1 }));
-// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-
-// const x = getUnresolvedBugs(store.getState());
-// const y = getUnresolvedBugs(store.getState());
-// console.log(x === y); // true
-
-// const bugs = getBugsByUser(1)(store.getState());
-// console.log(bugs);
+import * as actions from './store/api';
 
 const store = configureStore();
 
-// Dispatch function
-store.dispatch((dispatch, getState) => {
-  // Call an API
-  // When the promise is resolved => dispatch()
-  dispatch({ type: 'bugsReceived', bugs: [1, 2, 3] });
-  console.log(getState());
-  // If the promise is rejected => dispatch()
-});
+store.dispatch(
+  actions.apiCallBegan({
+    url: '/bugs',
+    onSuccess: 'bugsReceived',
+  })
+);
 
-store.dispatch({
-  type: 'error',
-  payload: { message: 'An error occurred.' },
-});
+// store.dispatch({
+//   type: 'apiCallBegan',
+//   payload: {
+//     url: '/bugs',
+//     onSuccess: 'bugsReceived',
+//     onError: 'apiRequestFailed',
+//   },
+// });
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,11 +4,12 @@ import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import logger from './middleware/logger';
 import func from './middleware/func';
 import toast from './middleware/toast';
+import api from './middleware/api';
 
 export default function () {
   // return configureStore({ reducer, middleware: [logger('console!'), func] });
   return configureStore({
     reducer,
-    middleware: [...getDefaultMiddleware(), logger('console!'), toast],
+    middleware: [...getDefaultMiddleware(), logger('console!'), toast, api],
   });
 }
