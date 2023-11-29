3f46a9191cba0aee9c9786b5c8bb614ecc334836
NicolasLi <linze201@gmail.com>
2023-11-29 15:25:33 +0800

    create new slice: projects

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,5 @@
-import reducer from './bugs';
+// import reducer from './bugs';
+import reducer from './projects';
 import { configureStore } from '@reduxjs/toolkit';
 
 export default function () {
