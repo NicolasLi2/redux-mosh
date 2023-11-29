24347305ccb0fdc146945dbdcae201c45a94557f
NicolasLi <linze201@gmail.com>
2023-11-29 12:23:32 +0800

    enable redux devtool

--- src/store.js
+++ src/store.js
@@ -1,6 +1,10 @@
 import { createStore } from 'redux';
 import reducer from './reducer';
 
-const store = createStore(reducer); // 'createStore' is deprecated.
+const store = createStore(
+  reducer,
+  // https://github.com/zalmoxisus/redux-devtools-extension#usage
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+); // 'createStore' is deprecated.
 
 export default store;
