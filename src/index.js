75eeb09f8dea2e49acb94f2910f34bdb5f187632
NicolasLi <linze201@gmail.com>
2023-11-29 15:57:59 +0800

    Selector

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import configureStore from './store/configureStore';
-import * as actions from './store/bugs';
+import { bugAdded, bugResolved, getUnresolvedBugs } from './store/bugs';
 import { projectAdded } from './store/projects';
 
 const store = configureStore();
@@ -10,8 +10,10 @@ store.subscribe(() => {
 
 store.dispatch(projectAdded({ name: 'Project 1' }));
 
-store.dispatch(actions.bugAdded({ description: 'Bug1' }));
-store.dispatch(actions.bugAdded({ description: 'Bug2' }));
-store.dispatch(actions.bugAdded({ description: 'Bug3' }));
-store.dispatch(actions.bugResolved({ id: 1 }));
-console.log(store.getState());
+store.dispatch(bugAdded({ description: 'Bug1' }));
+store.dispatch(bugAdded({ description: 'Bug2' }));
+store.dispatch(bugAdded({ description: 'Bug3' }));
+store.dispatch(bugResolved({ id: 1 }));
+
+const unresolvedBugs = getUnresolvedBugs(store.getState())
+console.log(unresolvedBugs);
