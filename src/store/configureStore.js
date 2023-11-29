bed5bf380e7778c3f79b99bbc0eca54b0cd79912
NicolasLi <linze201@gmail.com>
2023-11-29 17:47:25 +0800

    Dispatching Functions

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,13 @@
 // import reducer from './bugs';
 import reducer from './reducer';
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import logger from './middleware/logger';
+import func from './middleware/func';
 
 export default function () {
-  return configureStore({ reducer, middleware: [logger('console!')] });
+  // return configureStore({ reducer, middleware: [logger('console!'), func] });
+  return configureStore({
+    reducer,
+    middleware: [...getDefaultMiddleware(), logger('console!')],
+  });
 }
---
+++ src/store/middleware/func.js
@@ -0,0 +1,12 @@
+// Thunk
+
+// SNA
+const func =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    if (typeof action === 'function') action(dispatch, getState);
+    else next(action);
+  };
+
+export default func;
