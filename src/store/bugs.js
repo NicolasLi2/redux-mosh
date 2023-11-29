758bcb97f62af27ca458237c2e5b5cccb8e97b1f
NicolasLi <linze201@gmail.com>
2023-11-29 23:04:30 +0800

    Saving Data to the Server

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -3,8 +3,6 @@ import { createSelector } from 'reselect';
 import { apiCallBegan } from './api';
 import moment from 'moment';
 
-let lastId = 0;
-
 const slice = createSlice({
   name: 'bugs',
   initialState: {
@@ -30,11 +28,7 @@ const slice = createSlice({
       bugs.list[index].userId = userId;
     },
     bugAdded: (bugs, action) => {
-      bugs.list.push({
-        id: ++lastId,
-        description: action.payload.description,
-        resolved: false,
-      });
+      bugs.list.push(action.payload);
     },
     bugResolved: (bugs, action) => {
       const index = bugs.list.findIndex((bug) => bug.id === action.payload.id);
@@ -71,6 +65,14 @@ export const loadBugs = () => (dispatch, getState) => {
     })
   );
 };
+
+export const addBug = (bug) =>
+  apiCallBegan({
+    url,
+    method: 'post',
+    data: bug,
+    onSuccess: bugAdded.type,
+  });
 
 // // Selector
 // export const getUnresolvedBugs = (state) =>
