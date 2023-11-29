2226b758e932e284b9ffd7ce56f282e4e93081d0
NicolasLi <linze201@gmail.com>
2023-11-29 17:25:04 +0800

    parameterize a middleware function

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,5 +4,5 @@ import { configureStore } from '@reduxjs/toolkit';
 import logger from './middleware/logger';
 
 export default function () {
-  return configureStore({ reducer, middleware: [logger] });
+  return configureStore({ reducer, middleware: [logger('console!')] });
 }
