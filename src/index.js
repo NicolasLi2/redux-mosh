6cf28a75c09708458fa2df70ef42a18c10c1132a
NicolasLi <linze201@gmail.com>
2023-11-29 13:27:21 +0800

    use redux-toolkit to create actions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import configureStore from './store/configureStore';
 import * as actions from './store/bugs';
 
-const store = configureStore()
+const store = configureStore();
 
 store.subscribe(() => {
   console.log('Store changed!');
 });
 
-store.dispatch(actions.bugAdded('Bug1'));
-store.dispatch(actions.bugAdded('Bug2'));
-store.dispatch(actions.bugAdded('Bug3'));
-store.dispatch(actions.bugResolved(1));
+store.dispatch(actions.bugAdded({ description: 'Bug1' }));
+store.dispatch(actions.bugAdded({ description: 'Bug2' }));
+store.dispatch(actions.bugAdded({ description: 'Bug3' }));
+store.dispatch(actions.bugResolved({ id: 1 }));
 console.log(store.getState());
