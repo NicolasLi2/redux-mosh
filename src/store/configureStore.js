5f39e403d02012839f64df9207ca89cc8ab0c6c8
NicolasLi <linze201@gmail.com>
2023-11-29 17:21:00 +0800

    Creating Middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,8 @@
 // import reducer from './bugs';
 import reducer from './reducer';
 import { configureStore } from '@reduxjs/toolkit';
+import logger from './middleware/logger';
 
 export default function () {
-  return configureStore({ reducer });
+  return configureStore({ reducer, middleware: [logger] });
 }
