c04300d3d44de12e44aa967f18425704b2bcac13
NicolasLi <linze201@gmail.com>
2023-11-30 12:03:23 +0800

    Solitary Tests

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 import { createSelector } from 'reselect';
 import { apiCallBegan } from './api';
 import moment from 'moment';
+import axios from 'axios';
 
 const slice = createSlice({
   name: 'bugs',
@@ -66,13 +67,24 @@ export const loadBugs = () => (dispatch, getState) => {
   );
 };
 
-export const addBug = (bug) =>
-  apiCallBegan({
-    url,
-    method: 'post',
-    data: bug,
-    onSuccess: bugAdded.type,
-  });
+// make an api call
+// promise resolved => dispatch(success)
+export const addBug = async (bug) => {
+  try {
+    const response = await axios.post(url, bug);
+    dispatch(bugAdded(bug));
+  } catch (error) {
+    dispatch({ type: 'error' });
+  }
+};
+
+// export const addBug = (bug) =>
+//   apiCallBegan({
+//     url,
+//     method: 'post',
+//     data: bug,
+//     onSuccess: bugAdded.type,
+//   });
 
 export const resolveBug = (id) =>
   apiCallBegan({
