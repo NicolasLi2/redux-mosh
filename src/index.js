be86f027d105068dddee0f1801801336132cf627
NicolasLi <linze201@gmail.com>
2023-11-30 11:05:02 +0800

    end of Consuming APIs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import configureStore from './store/configureStore';
 import { loadBugs } from './store/bugs';
-import { addBug } from './store/bugs';
+import { addBug, resolveBug, assignBugToUser } from './store/bugs';
 
 const store = configureStore();
 
 // UI Layer
-// store.dispatch(loadBugs());
-store.dispatch(addBug({ description: 'a' }));
+store.dispatch(loadBugs());
+setTimeout(() => store.dispatch(resolveBug(1)), 2000);
+setTimeout(() => store.dispatch(assignBugToUser(1,4)), 2000);
